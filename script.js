6033b19c9d5d8294d879a9e3164ad48d21094a38
oscartorrest-prog <oscar.torres.t@gmail.com>
2025-08-25 06:08:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    e.preventDefault();
+    const id = a.getAttribute('href');
+    const el = document.querySelector(id);
+    if(el){ el.scrollIntoView({behavior:'smooth'}); }
+  });
+});
